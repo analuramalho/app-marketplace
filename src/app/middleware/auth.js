45c1fd999ea77d5db7e2546bb767b46a0b6e6e98
Ana Luiza Ramalho <analuizaramalho9@gmail.com>
2020-06-30 23:55:01 +0000

    criando middleware de auth

---
+++ src/app/middleware/auth.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken')
+const authConfig=require('../../config/auth')
+const { promisify } = require('util')
+
+module.exports = async (req,res,next)=>{
+    const authHeaders = req.headers.authorization
+    
+    if(!authHeaders){
+        return res.status(401).json({ error: 'Token not provider' })
+    }
+
+    const[, token]= authHeaders.split(' ')
+
+    try{
+       const decoded = await promisify(jwt.verify)(token, authConfig.secret)
+       req.userId = decoded.userId
+       return next()
+    }
+    catch(err){
+        return res.status(401).json({error: 'Token Invalid'})
+    }
+}
--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,8 @@ const express = require('express')
 
 const routes = express.Router()
 
+const authMiddleware = require('./app/middleware/auth')
+
 
 const UserController = require('./app/controllers/UserController')
 const SessionController = require('./app/controllers/SessionController')
@@ -10,6 +12,8 @@ const SessionController = require('./app/controllers/SessionController')
 
 routes.post('/users', UserController.store)
 routes.post('/sessions', SessionController.store)
+
+routes.get('/teste',authMiddleware,(req,res)=>res.json({ok:true }))
 
 
 module.exports = routes
