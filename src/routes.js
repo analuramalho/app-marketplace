3300b2fcaca8ef8a5b3c086251a502b5b852fdcf
Ana Luiza Ramalho <analuizaramalho9@gmail.com>
2020-07-13 20:19:16 +0000

    Configurando validações

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const validate=require('express-validation')
 
 const routes = express.Router()
 
@@ -6,22 +7,23 @@ const authMiddleware = require('./app/middleware/auth')
 
 
 const controllers = require('./app/controllers')
+const validators = require('./app/validators')
 
 
-routes.post('/users', controllers.UserController.store)
-routes.post('/sessions', controllers.SessionController.store)
+routes.post('/users', validate(validators.User),controllers.UserController.store)
+routes.post('/sessions',validate(validators.Session), controllers.SessionController.store)
 
 routes.use(authMiddleware)//todas as rotas apartir daqui precisam estar auteticado para acessar
 
 routes.get('/ads',controllers.AdController.index)
 routes.get('/ads/:id',controllers.AdController.show)
-routes.post('/ads',controllers.AdController.store)
-routes.put('/ads/:id',controllers.AdController.update)
+routes.post('/ads',validate(validators.Ad),controllers.AdController.store)
+routes.put('/ads/:id',validate(validators.Ad),controllers.AdController.update)
 routes.delete('/ads/:id',controllers.AdController.destroy)
 
 
 
-routes.post('/purchase',controllers.PurchaseController.store)
+routes.post('/purchase',validate(validators.Purchase),controllers.PurchaseController.store)
 
 
 
